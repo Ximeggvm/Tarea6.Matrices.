3036d911e90e552b4612a066c35abcad3919ee5a
Ximeggvm <110927159+Ximeggvm@users.noreply.github.com>
2022-10-07 12:30:27 -0500

    Update exercise.py

--- assignments/13Determinante/src/exercise.py
+++ assignments/13Determinante/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+us=input()
+res=us.split()
 
+us2=input()
+res2=us2.split()
+
+if len(res)!=2:
+    print('La matriz no es una matriz de 2x2')
+else:
+    mat=[[int(res[0]),int(res[1])],[int(res2[0]),int(res2[1])]]
+    res=(int(res[0])*int(res2[1]))-((int(res[1])*int(res2[0])))
+    print(res)
 if __name__=='__main__':
     main()
